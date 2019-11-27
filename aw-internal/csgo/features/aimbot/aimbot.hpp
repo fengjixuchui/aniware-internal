979b6fa3186511bc635c34cfc9a7e8c53d875b63
BambooFox <stian.ostby@gmail.com>
2019-11-27 17:57:55 +0100

    fixed some hooks removed aimbot updated fsn

--- aw-internal/csgo/features/aimbot/aimbot.hpp
+++ aw-internal/csgo/features/aimbot/aimbot.hpp
@@ -2,34 +2,5 @@
 
 namespace aimbot
 {
-	struct AimbotData
-	{
-		AimbotData(player_t* pl, int i, int dmg, math::vec3_t p, float dist) :
-			player(pl),
-			index(i),
-			damage(dmg),
-			best_point(p),
-			distance(dist)
-		{};
 
-		math::vec3_t best_point;
-		player_t* player;
-		int index;
-		int damage;
-		float distance;
-	};
-
-	int shots_fired;
-
-	player_t* last_target = nullptr;
-	std::vector<AimbotData> players;
-
-	void choose_angles();
-	void select_target();
-	void restore_players();
-	bool get_points_from_hitbox();
-	void quickstop();
-	bool is_valid();
-	bool hitchance();
-	void work();
 }
--- aw-internal/csgo/hooks/functions/frame_stage_notify.cpp
+++ aw-internal/csgo/hooks/functions/frame_stage_notify.cpp
@@ -5,8 +5,14 @@ void __fastcall hook_handler_t::frame_stage_notify( REGISTERS, client_frame_stag
 	switch ( stage )
 	{
 		case FRAME_RENDER_START:
-			ctx::csgo.engine->ClientCmd_Unrestricted( "say FRAME_RENDER_START" );
-			break;
+		{
+
+		} break;
+
+		case FRAME_NET_UPDATE_START:
+		{
+
+		} break;
 	}
 
 	return hooks::get().orig_frame_stage_notify( ecx, edx, stage );
